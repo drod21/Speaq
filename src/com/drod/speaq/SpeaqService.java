5320bf06743baa585567c321db0ff2cb3835a374
Derek <drod2169@gmail.com>
2013-09-14 11:33:38 -0400

    Updates. Add SpeaqService

---
+++ src/com/drod/speaq/SpeaqService.java
@@ -0,0 +1,32 @@
+package com.drod.speaq;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SpeaqService extends IntentService {
+
+	public SpeaqService() {
+		super("SpeaqService");
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+	    long endTime = System.currentTimeMillis() + 5*1000;
+	      while (System.currentTimeMillis() < endTime) {
+	          synchronized (this) {
+	              try {
+	                  wait(endTime - System.currentTimeMillis());
+	              } catch (Exception e) {
+	              }
+	          }
+	      }
+	}
+
+}
