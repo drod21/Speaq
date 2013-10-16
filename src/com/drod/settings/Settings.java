ad7343adc0e2edfbdb82923889c3cf82691b2765
Derek <drod2169@gmail.com>
2013-10-16 08:36:14 -0400

    Updates to settings

--- src/com/drod/settings/Settings.java
+++ src/com/drod/settings/Settings.java
@@ -1,9 +1,11 @@
 package com.drod.settings;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class Settings extends Activity {
+public class Settings extends PreferenceActivity {
+    public static final String KEY_PREF_SYNC_CONN = "pref_syncConnectionType";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -11,5 +13,4 @@ public class Settings extends Activity {
 		getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new SettingsFragment()).commit();
 	}
-
 }
